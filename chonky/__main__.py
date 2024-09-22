d086119b32a867a4a577af4fbe6ed506a847b627
James Dolan <jdolan@gmail.com>
2024-09-22 17:25:05 -0400

    recursive workspaces

--- chonky/__main__.py
+++ chonky/__main__.py
@@ -1,9 +1,21 @@
+import os
 import sys
 
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Generator
 
 from chonky import Client, ClientError
+
+def locate_configs(workspace_root: Path = Path.cwd(), config_name: str = "CHONKY") -> Generator[Path, None, None]:
+    for root, dirs, files in os.walk(workspace_root):
+        dirs[:] = [
+            d
+            for d in dirs
+            if not d.startswith((".","__"))
+        ]
+        if config_name in files:
+            yield Path(root).relative_to(workspace_root) / config_name
 
 def main() -> None:
     COMMANDS = {
@@ -13,12 +25,19 @@ def main() -> None:
         "revert":   Client.revert,
     }
     parser = ArgumentParser()
-    parser.add_argument("--config", type=Path, default=Path.cwd() / "CHONKY")
+    parser.add_argument("--config", type=Path, default=None)
     parser.add_argument("command", choices=COMMANDS.keys())
     args = parser.parse_args()
+    configs = [args.config] if args.config else list(locate_configs())
+    if not configs:
+        print("No workspaces found", file=sys.stderr)
     try:
-        client = Client(args.config)
-        COMMANDS[args.command](client)
+        command = COMMANDS[args.command]
+        for i,config in enumerate(configs):
+            if i > 0: print("") # line break between workspaces
+            print(f"Workspace: {config}")
+            client = Client(config)
+            command(client)
     except ClientError as e:
         print(e, file=sys.stderr)
         quit(1)
